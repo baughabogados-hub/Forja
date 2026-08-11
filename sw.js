8db7c605b91b679b040e27f0755a33f572cb8eea
baughabogados-hub <baugh.abogados@gmail.com>
2026-08-11 10:06:05 -0600

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'forja-v2';
+const CACHE = 'forja-v3';
 const ASSETS = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', (e) => {
